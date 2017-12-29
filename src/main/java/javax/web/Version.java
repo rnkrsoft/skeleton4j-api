b5bd82b14bbc06c6456d16c62245ed838ef3a103
wing4j <wing4j@foxmail.com>
2017-12-29 08:37:50 +0800

    增加版本号

---
+++ src/main/java/javax/web/Version.java
@@ -0,0 +1,23 @@
+package javax.web;
+
+/**
+ * Created by devops4j on 2017/12/29.
+ * 该类申明了Skeleton4j API的版本号
+ */
+public class Version {
+    /**
+     * 版本信息
+     * @return 版本号
+     */
+    public static String version(){
+        return "1.0.0";
+    }
+
+    /**
+     * 是否为正式版本
+     * @return
+     */
+    public static boolean release(){
+        return false;
+    }
+}
