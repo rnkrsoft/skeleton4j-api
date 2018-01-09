2b3841a6d037b726b113830fdc71bc61d26107c3
wing4j <wing4j@foxmail.com>
2018-01-09 10:40:59 +0800

    修改

--- src/main/java/javax/web/skeleton4j/Skeleton4jService.java
+++ src/main/java/javax/web/skeleton4j/Skeleton4jService.java
@@ -17,17 +17,19 @@ import java.util.Collection;
 public interface Skeleton4jService {
     /**
      * 获取当前配置对象
+     *
      * @return
      */
     Skeleton4jConfig getConfig();
 
     /**
      * 初始化核心服务
-     * @param config 配置对象
-     * @param docScanner 文档扫描器 如果不存在，则自动使用DocScannerFactory创建实例
-     * @param modulePool 模块池 如果模块池不存在则自动创建
-     * @param pagePool 页面池 如果页面池不存在则自动创建
-     * @param pagePackages 页面包路径 页面路径默认需要包含javax.web.skeleton4j.page
+     *
+     * @param config        配置对象
+     * @param docScanner    文档扫描器 如果不存在，则自动使用DocScannerFactory创建实例
+     * @param modulePool    模块池 如果模块池不存在则自动创建
+     * @param pagePool      页面池 如果页面池不存在则自动创建
+     * @param pagePackages  页面包路径 页面路径默认需要包含javax.web.skeleton4j.page
      * @param themePackages 主题包路径 主题路径默认需要包含javax.web.skeleton4j.theme
      */
     void init(Skeleton4jConfig config, DocScanner docScanner, ModulePool modulePool, PagePool pagePool, Collection<String> pagePackages, Collection<String> themePackages);
@@ -36,11 +38,15 @@ public interface Skeleton4jService {
      * 扫描所有页面类
      */
     void scan();
+
     /**
      * 编译页面类为代码
+     *
      * @param pageClass 页面类
+     * @return 生成页面文件路径
      */
-    void compile(Class pageClass);
+    File compile(Class pageClass);
+
     /**
      * @param product  模块名称 模块名即权限中的product
      * @param action   页面名称 模块名即权限中的action
@@ -75,7 +81,7 @@ public interface Skeleton4jService {
      * @param action    类名缩写(action)
      * @param subAction 交易别名(subAction).通过模块中得接口定义原始类找到真实得接口，接口别名能够转换为接口名字和版本号
      * @param data      JSON 报文
-     * @param file   导入文件
+     * @param file      导入文件
      * @param request   HTTP请求对象
      * @param response  HTTP应答对象
      */
