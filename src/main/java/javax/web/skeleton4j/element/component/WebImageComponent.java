2dcb8213b76c0a9cef238fa0010c8c3cf3bb64b0
wing4j <wing4j@foxmail.com>
2018-01-14 12:13:39 +0800

    修改

--- src/main/java/javax/web/skeleton4j/element/component/WebImageComponent.java
+++ src/main/java/javax/web/skeleton4j/element/component/WebImageComponent.java
@@ -5,4 +5,39 @@ package javax.web.skeleton4j.element.component;
  * 图片组件
  */
 public interface WebImageComponent extends WebFormableComponent , WebGroupableComponent{
+    /**
+     * 图片是否支持缩放
+     * @param zoom 是否支持缩放
+     */
+    void setZoom(boolean zoom);
+
+    /**
+     * 是否支持缩放
+     * @return 是否支持缩放
+     */
+    boolean isZoom();
+
+    /**
+     * 设置图片展示高度
+     * @param displayHeight
+     */
+    void setDisplayHeight(int displayHeight);
+
+    /**
+     * 获取图片展示高度
+     * @return
+     */
+    int getDisplayHeight();
+
+    /**
+     * 设置图片展示宽度
+     * @param displayWidth
+     */
+    void setDisplayWidth(int displayWidth);
+
+    /**
+     * 获取图片展示宽度
+     * @return
+     */
+    int getDisplayWidth();
 }
