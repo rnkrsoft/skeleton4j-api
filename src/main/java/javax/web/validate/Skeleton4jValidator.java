3a1f31a6920d6d5bb069d933c9a2214372f87e2e
wing4j <wing4j@foxmail.com>
2018-01-18 16:17:47 +0800

    修改校验框架

--- src/main/java/javax/web/validate/Skeleton4jValidator.java
+++ src/main/java/javax/web/validate/Skeleton4jValidator.java
@@ -1,5 +1,8 @@
 package javax.web.validate;
 
+import javax.web.doc.Responseable;
+import javax.web.doc.RspCodeWritable;
+
 /**
  * Created by devops4j on 2018/1/4.
  * 校验器
@@ -13,6 +16,13 @@ public interface Skeleton4jValidator {
      */
     boolean validate(Object value, boolean throwException);
 
+    /**
+     * 校验值对象，如果校验不通过，则将rspCode和rspDesc填充到Response
+     * @param value 校验对象
+     * @param response 应答对象
+     * @return 校验通过返回真，否则返回假
+     */
+    boolean validate(Object value, RspCodeWritable response);
     /**
      * 通过校验上下文进行校验值对象
      * @param context 校验上下文，可以设置发生校验不通过，是否抛出异常
--- src/test/java/javax/web/validate/DemoSkeleton4jValidator.java
+++ src/test/java/javax/web/validate/DemoSkeleton4jValidator.java
@@ -1,5 +1,7 @@
 package javax.web.validate;
 
+import javax.web.doc.RspCodeWritable;
+
 /**
  * Created by devops4j on 2018/1/4.
  */
@@ -7,6 +9,11 @@ public class DemoSkeleton4jValidator implements Skeleton4jValidator{
     @Override
     public boolean validate(Object value, boolean throwException) {
         System.out.println(value);
+        return false;
+    }
+
+    @Override
+    public boolean validate(Object value, RspCodeWritable response) {
         return false;
     }
 
