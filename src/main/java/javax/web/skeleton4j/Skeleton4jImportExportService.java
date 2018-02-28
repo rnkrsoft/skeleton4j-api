be2e1f2839f2f91420bd7191776b696a9db7882c
wing4j <wing4j@foxmail.com>
2018-02-28 18:01:35 +0800

    修改

---
+++ src/main/java/javax/web/skeleton4j/Skeleton4jImportExportService.java
@@ -0,0 +1,51 @@
+package javax.web.skeleton4j;
+
+import javax.web.doc.InterfaceInfo;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by devops4j on 2018/2/28.
+ * 导入导出服务
+ */
+public interface Skeleton4jImportExportService {
+    /**
+     * 支持的文件格式
+     * @return 文件格式
+     */
+    String getSupportFormat();
+    /**
+     * 解析接口对应的文件
+     * @param interfaceInfo 接口元信息
+     * @param file 文件
+     * @param <T> 解析出的对象
+     * @return 解析出的对象
+     */
+    <T> T parse(InterfaceInfo interfaceInfo, File file) throws IOException;
+
+    /**
+     * 解析接口对应的输入流
+     * @param interfaceInfo 接口元信息
+     * @param is 输入流
+     * @param <T> 解析出的对象
+     * @return 解析出的对象
+     */
+    <T> T parse(InterfaceInfo interfaceInfo, InputStream is) throws IOException;
+
+    /**
+     * 根据接口和输入的对象生成导出文件
+     * @param interfaceInfo 接口元信息
+     * @param file 文件
+     * @param data 对象
+     */
+    void generate(InterfaceInfo interfaceInfo, File file, Object data) throws IOException;
+    /**
+     * 根据接口和输入的对象生成输出流
+     * @param interfaceInfo 接口元信息
+     * @param os 输出流
+     * @param data 对象
+     */
+    void generate(InterfaceInfo interfaceInfo, OutputStream os, Object data) throws IOException;
+}
---
+++ src/main/java/javax/web/skeleton4j/Skeleton4jImportExportServiceFactory.java
@@ -0,0 +1,49 @@
+package javax.web.skeleton4j;
+
+import com.devops4j.logtrace4j.ErrorContext;
+import com.devops4j.logtrace4j.ErrorContextFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * Created by devops4j on 2018/2/28.
+ */
+@Slf4j
+public class Skeleton4jImportExportServiceFactory {
+    /**
+     * 根据文件格式获取导入导出服务
+     * @param format 文件格式，支持csv,xls等
+     * @return 导入导出服务
+     */
+    public static Skeleton4jImportExportService getInstance(String format){
+        Skeleton4jImportExportService service = null;
+        ServiceLoader<Skeleton4jImportExportService> serviceLoader = ServiceLoader.load(Skeleton4jImportExportService.class);
+        Iterator<Skeleton4jImportExportService> serviceIterator = serviceLoader.iterator();
+        while (service == null && serviceIterator.hasNext()) {
+            Skeleton4jImportExportService service0 = serviceIterator.next();
+            if (format == null) {
+                log.warn(" 文件格式为空，使用默认格式xls");
+                format = "xls";
+            }
+            if (format.equalsIgnoreCase(service0.getSupportFormat())) {
+                service = service0;
+            }
+        }
+        if (service == null) {
+            ErrorContext errorContext = ErrorContextFactory.instance().reset()
+                    .message("未发现支持文件格式 '{}' 的实现", format)
+                    .solution("在META-INF/services/javax.web.skeleton4j.Skeleton4jConfigService");
+            Iterator<Skeleton4jImportExportService> it = serviceLoader.iterator();
+            int i = 0;
+            while (it.hasNext()) {
+                i++;
+                Skeleton4jImportExportService service0 = it.next();
+                errorContext.extra("found", "实现[{}] {} 支持格式 '{}'", i, service0.getClass().getName(), service0.getSupportFormat());
+            }
+            throw errorContext.runtimeException();
+        }
+        return service;
+    }
+}
