4743618b6dd6f6b52fcb183479fcd508bea8f689
devops4j <devops4j@devops4j.com>
2018-07-01 09:26:01 +0800

    修改

--- src/main/java/javax/web/skeleton4j/Skeleton4jService.java
+++ src/main/java/javax/web/skeleton4j/Skeleton4jService.java
@@ -1,14 +1,6 @@
 package javax.web.skeleton4j;
 
 
-import javax.web.doc.DocScanner;
-import javax.web.skeleton4j.config.Skeleton4jConfig;
-import javax.web.skeleton4j.pool.ModulePool;
-import java.io.File;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-
 /**
  * Created by rnkrsoft.com on 2017/12/16.
  * 核心服务
