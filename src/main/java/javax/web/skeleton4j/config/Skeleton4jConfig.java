004226e64ac50e37fcef18eecf7507abe48f9067
devops4j <devops4j@devops4j.com>
2018-08-12 12:24:22 +0800

    修改

--- src/main/java/javax/web/skeleton4j/config/Skeleton4jConfig.java
+++ src/main/java/javax/web/skeleton4j/config/Skeleton4jConfig.java
@@ -75,6 +75,7 @@ public interface Skeleton4jConfig {
      * 网页组件注册对象类名
      */
     String WEB_COMPONENT_REGISTRY_CLASS_NAME = "webComponentRegistryClassName";
+    String SKELETON4J_SERVICE_CLASS_NAME = "skeleton4jServiceClassName";
     /**
      * 列出所有参数的参数名
      *
