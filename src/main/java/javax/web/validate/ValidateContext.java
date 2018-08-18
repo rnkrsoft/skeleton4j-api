802913572f8e1249e7e2cbc419a95f3d83e031a1
devops4j <devops4j@devops4j.com>
2018-08-18 11:15:58 +0800

    修改

--- src/main/java/javax/web/validate/ValidateContext.java
+++ src/main/java/javax/web/validate/ValidateContext.java
@@ -21,6 +21,10 @@ public class ValidateContext {
      */
     String failureField;
     /**
+     * 失败字段名称
+     */
+    String failureFieldName;
+    /**
      * 对象包装
      */
     MetaObject metaObject;
