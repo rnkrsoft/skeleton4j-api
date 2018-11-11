488ed3500a08352bfadd59565bbe31073acf1ae8
devops4j <devops4j@devops4j.com>
2018-11-11 22:35:33 +0800

    重构Api包

--- src/main/java/javax/web/skeleton4j/element/component/WebMapAreaComponent.java
+++ src/main/java/javax/web/skeleton4j/element/component/WebMapAreaComponent.java
@@ -4,6 +4,6 @@ package javax.web.skeleton4j.element.component;
  * Created by rnkrsoft.com on 2018/11/11.
  * 地图区域组件，用于对地图上标注的电子栅栏进行展示
  */
-public interface WebMapRegionComponent extends WebFormableComponent{
+public interface WebMapAreaComponent extends WebFormableComponent{
 
 }
